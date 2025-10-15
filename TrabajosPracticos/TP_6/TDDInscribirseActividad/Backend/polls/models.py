2f7d4ce1fb2d276729e266fcb3969c422129c72d
JuanAngonese <juan.pablo.angonese@gmail.com>
2025-10-15 19:38:28 -0300

    Base de datos y validación en models

--- TrabajosPracticos/TP_6/TDDInscribirseActividad/Backend/polls/models.py
+++ TrabajosPracticos/TP_6/TDDInscribirseActividad/Backend/polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+import datetime
+from django.utils import timezone
 # Create your models here.
 
 class Inscripcion(models.Model):
@@ -10,7 +11,24 @@ class Inscripcion(models.Model):
     edad = models.IntegerField()
     cant_personas = models.IntegerField()
     talle = models.CharField(max_length=10, blank=True, null=True)
-    terminosYcondiciones = models.BooleanField()
 
     def __str__(self):
         return f"{self.nombre} {self.dni} - Actividad {self.id_actividad}"
+    
+class Actividad(models.Model):
+    id = models.AutoField(primary_key=True)
+    tipo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha = models.DateField()
+    hora = models.TimeField()
+    cupo_disponible = models.IntegerField()
+    
+    def __str__(self):
+        return self.tipo
+
+    def esta_disponible(self):
+        hoy = timezone.now().date()
+        cinco_minutos_despues = (datetime.datetime.now() + datetime.timedelta(minutes=5)).time()
+        return self.cupo_disponible > 0 and self.fecha >= hoy and self.hora >= cinco_minutos_despues
+    
+    

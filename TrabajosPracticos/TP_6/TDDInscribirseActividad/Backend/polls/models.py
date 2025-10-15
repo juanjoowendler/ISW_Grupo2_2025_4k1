4b2b096a689a8385e8bd37ae663b3e719d7631ee
JuanAngonese <juan.pablo.angonese@gmail.com>
2025-10-15 18:34:56 -0300

    Se realizó el models

--- TrabajosPracticos/TP_6/TDDInscribirseActividad/Backend/polls/models.py
+++ TrabajosPracticos/TP_6/TDDInscribirseActividad/Backend/polls/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+'''
+            "id_actividad":data[0]["id_actividad"],
+            "nombre":"Emanuel Scrosati",
+            "DNI":45074602,
+            "edad":22,
+            "cant_personas":1,
+            "talle":"XL",
+            "terminosYcondiciones":True
+'''
+class Inscripcion(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_actividad = models.IntegerField()
+    nombre = models.CharField(max_length=100)
+    dni = models.IntegerField()
+    edad = models.IntegerField()
+    cant_personas = models.IntegerField()
+    talle = models.CharField(max_length=10, blank=True, null=True)
+    terminosYcondiciones = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.dni} - Actividad {self.id_actividad}"
